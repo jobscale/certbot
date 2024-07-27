9c21d0c8095167d6b249883b94b4138ab4e2740d
jobscale <jobscale@users.noreply.github.com>
2024-07-27 11:42:23 +0900

    cleanup updated

--- ddns-hook-auth.sh
+++ ddns-hook-auth.sh
@@ -6,6 +6,6 @@ set -eu
   env | grep CERTBOT
   CERTBOT_AUTH=true CERTBOT_DOMAIN=_acme-challenge.${CERTBOT_DOMAIN} \
   ENV=dev node app
-  dig _acme-challenge.${CERTBOT_DOMAIN}
+  dig _acme-challenge.${CERTBOT_DOMAIN} txt
   echo
 }
--- ddns-hook-cleanup.sh
+++ ddns-hook-cleanup.sh
@@ -5,8 +5,8 @@ set -eu
   sleep ${CERTBOT_REMAINING_CHALLENGES}.9
   echo '[CERTBOT - CLEANUP]'
   env | grep CERTBOT
-  CERTBOT_CLEANUP=true CERTBOT_DOMAIN=_acme-challenge.${CERTBOT_DOMAIN} \
-  ENV=dev node app
-  dig _acme-challenge.${CERTBOT_DOMAIN}
+  # CERTBOT_CLEANUP=true CERTBOT_DOMAIN=_acme-challenge.${CERTBOT_DOMAIN} \
+  # ENV=dev node app
+  dig _acme-challenge.${CERTBOT_DOMAIN} txt
   echo
 }
