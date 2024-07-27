4a49db808447df76b1c56bab6853da04011f5faf
jobscale <jobscale@users.noreply.github.com>
2024-07-27 15:19:03 +0900

    ddns hook updated

--- ddns-hook-auth.sh
+++ ddns-hook-auth.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 set -eu
 
+dig-all() {
+  sleep 120
+  for domain in $(echo $CERTBOT_ALL_DOMAINS | sed -e 's/,/ /g' )
+  do
+    dig _acme-challenge.${domain} txt | grep -A 2 'ANSWER SECTION' | grep -w 'TXT'
+  done
+}
+
 {
-  sleep ${CERTBOT_REMAINING_CHALLENGES}.9
-  echo '[CERTBOT - AUTH]'
+  TIME=$(( 24 - ${CERTBOT_REMAINING_CHALLENGES} )).9
+  echo "[CERTBOT - AUTH] TIME = $TIME"
+  sleep $TIME
   env | grep CERTBOT
   CERTBOT_AUTH=true CERTBOT_DOMAIN=_acme-challenge.${CERTBOT_DOMAIN} \
   ENV=dev node app
-  sleep 120
-  dig _acme-challenge.${CERTBOT_DOMAIN} txt
+  echo
+  [[ "$CERTBOT_REMAINING_CHALLENGES" == "0" ]] && dig-all
   echo
 }
--- ddns-hook-cleanup.sh
+++ ddns-hook-cleanup.sh
@@ -2,11 +2,7 @@
 set -eu
 
 {
-  sleep ${CERTBOT_REMAINING_CHALLENGES}.9
-  echo '[CERTBOT - CLEANUP]'
+  echo -e '\n\n[CERTBOT - CLEANUP]\n\n'
   env | grep CERTBOT
-  # CERTBOT_CLEANUP=true CERTBOT_DOMAIN=_acme-challenge.${CERTBOT_DOMAIN} \
-  # ENV=dev node app
-  dig _acme-challenge.${CERTBOT_DOMAIN} txt
   echo
 }
