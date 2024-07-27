2673ce8962f62a34464f44f46feb97ba9381d8b7
jobscale <jobscale@users.noreply.github.com>
2024-07-27 11:56:37 +0900

    wait before update dns

--- ddns-hook-auth.sh
+++ ddns-hook-auth.sh
@@ -2,10 +2,12 @@
 set -eu
 
 {
+  sleep ${CERTBOT_REMAINING_CHALLENGES}.9
   echo '[CERTBOT - AUTH]'
   env | grep CERTBOT
   CERTBOT_AUTH=true CERTBOT_DOMAIN=_acme-challenge.${CERTBOT_DOMAIN} \
   ENV=dev node app
+  sleep 120
   dig _acme-challenge.${CERTBOT_DOMAIN} txt
   echo
 }
