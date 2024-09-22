30b61849b4b83d7ed2877afc91f826e6d6082540
jobscale <jobscale@users.noreply.github.com>
2024-09-22 21:44:16 +0900

    wait updated

--- ddns-hook-auth.sh
+++ ddns-hook-auth.sh
@@ -35,7 +35,7 @@ time {
     do
       dig _acme-challenge.${domain} txt | grep -A 2 'ANSWER SECTION' | grep -w 'TXT'
     done
-    sleep 600
+    sleep 120
   }
   echo
 } | tee -a CHALLENGE.${CERTBOT_DOMAIN}.log
