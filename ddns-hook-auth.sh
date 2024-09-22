36cdd51fed44856977b87f6c20ea94d363d1f9dd
jobscale <jobscale@users.noreply.github.com>
2024-09-22 21:52:51 +0900

    MULTIPLE=allow added

--- ddns-hook-auth.sh
+++ ddns-hook-auth.sh
@@ -26,7 +26,7 @@ time {
   SUB=$(echo "${CHALLENGE}" | sed -e 's/jsx\.jp$//' | sed -e 's/\.$//')
   DOMAIN=${SUB} TOKEN=$(date +%s) \
   TYPE=TXT R_DATA="${CERTBOT_VALIDATION}" \
-  ENV=dev node app
+  MULTIPLE=allow ENV=dev node app
   echo
   dig-all "${CHALLENGE}"
   echo
