013645d7cfb4dd9a742cc054ab20e4ae18f3f338
jobscale <jobscale@users.noreply.github.com>
2026-06-20 11:02:54 +0900

    output color added

--- ddns-hook-auth.sh
+++ ddns-hook-auth.sh
@@ -5,9 +5,9 @@ dig-all() {
   CHALLENGE="$1"
   echo
   echo -n "wait for DNS n seconds - "
-  for i in {10..1}
+  for i in {30..1}
   do
-    sleep 10
+    sleep 4
     echo -n "$i .. "
     ANSWER=$(
       dig ${CHALLENGE} txt | grep -A 2 'ANSWER SECTION' | grep -w 'TXT' | grep -- "${CERTBOT_VALIDATION}" | wc -l
@@ -20,7 +20,7 @@ dig-all() {
 }
 
 auth() {
-  echo " $(TZ=Asia/Tokyo date -Iseconds) === [CERTBOT - AUTH] ${CERTBOT_DOMAIN} (${CERTBOT_REMAINING_CHALLENGES}) ==="
+  echo "\e[33m $(TZ=Asia/Tokyo date -Iseconds) === [CERTBOT - AUTH] ${CERTBOT_DOMAIN} (${CERTBOT_REMAINING_CHALLENGES}) ===\e[0m"
   echo
   env | grep CERTBOT
   echo
--- ddns-hook-cleanup.sh
+++ ddns-hook-cleanup.sh
@@ -2,12 +2,12 @@
 set -eu
 
 cleanup() {
-  echo " $(TZ=Asia/Tokyo date -Iseconds) === [CERTBOT - CLEANUP] ${CERTBOT_DOMAIN} (${CERTBOT_REMAINING_CHALLENGES}) ==="
+  echo "\e[33m $(TZ=Asia/Tokyo date -Iseconds) === [CERTBOT - CLEANUP] ${CERTBOT_DOMAIN} (${CERTBOT_REMAINING_CHALLENGES}) ===\e[0m"
   echo
   env | grep CERTBOT
-  echo "before delete short: $(dig ${CHALLENGE} txt +short)"
   echo
   CHALLENGE=_acme-challenge.${CERTBOT_DOMAIN}
+  echo "before delete short: $(dig ${CHALLENGE} txt +short)"
   SUB=$(echo "${CHALLENGE}" | sed -e 's/jsx\.jp$//' | sed -e 's/\.$//')
   DOMAIN=${SUB} TOKEN=$(date +%s) \
   TYPE=TXT R_DATA="${CERTBOT_VALIDATION}" \
